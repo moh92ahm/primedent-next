29d6fb56371006b71c8b6f77d4473f94322945eb
moh92ahm <mohamad92ahm@gmail.com>
2025-07-08 13:36:50 +0300

    some morrre modifications

--- src/components/Header/HeaderClient.tsx
+++ src/components/Header/HeaderClient.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { useHeaderTheme } from '@/providers/HeaderTheme'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
@@ -15,18 +14,8 @@ import { Menu } from 'lucide-react'
 export const HeaderClient: React.FC = () => {
   /* Storing the value in a useState to avoid hydration errors */
   const [theme, setTheme] = useState<string | null>(null)
-  const { headerTheme, setHeaderTheme } = useHeaderTheme()
   const pathname = usePathname()
 
-  useEffect(() => {
-    setHeaderTheme(null)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname])
-
-  useEffect(() => {
-    if (headerTheme && headerTheme !== theme) setTheme(headerTheme)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [headerTheme])
 
   return (
     <header className="container relative z-20 max-w-screen-xl" {...(theme ? { 'data-theme': theme } : {})}>
