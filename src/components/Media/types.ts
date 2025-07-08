a3c46205c6d5f17b70df6159367b405f17511a98
moh92ahm <mohamad92ahm@gmail.com>
2025-07-08 14:20:04 +0300

    media updated

--- src/components/Media/types.ts
+++ src/components/Media/types.ts
@@ -1,7 +1,6 @@
 import type { StaticImageData } from 'next/image'
 import type { ElementType, Ref } from 'react'
 
-import type { Media as MediaType } from '@/payload-types'
 
 export interface Props {
   alt?: string
@@ -15,7 +14,7 @@ export interface Props {
   loading?: 'lazy' | 'eager' // for NextImage only
   priority?: boolean // for NextImage only
   ref?: Ref<HTMLImageElement | HTMLVideoElement | null>
-  resource?: MediaType | string | number | null // for Payload media
+  resource?: string | number | Record<string, any> | null // for Payload media
   size?: string // for NextImage only
   src?: StaticImageData // for static media
   videoClassName?: string
